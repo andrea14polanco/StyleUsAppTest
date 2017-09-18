ac2b7b7fe37ae2d3e69576622710f98a5fc2225e
migueloc19 <migueloviedoc@gmail.com>
2017-09-17 18:11:22 -0700

    Get y Set de campos de la clase user

--- src/main/java/hello/User.java
+++ src/main/java/hello/User.java
@@ -2,23 +2,34 @@ package hello;
 
 public class User {
 
-    private long id;
-    private String content;
-//
+	private long id;
+	private String username;
+    private String password;
+    private String email;
+	
     public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
 }
